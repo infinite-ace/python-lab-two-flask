c146dbb2ef04c645137bc1d26f853c0f2cd18536
infinite-ace <angelkukushev@infinitelambda.com>
2021-11-23 16:07:20 +0200

    adds library and example request method

--- flask-lab.py
+++ flask-lab.py
@@ -1,2 +1,11 @@
 from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def greet():
+    return "<h1> Greetings, programmer! </h1> "
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
