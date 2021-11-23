f3a629962d66375305e1fb6b51afa5cc55c8996b
Angel Kukushev <angelkukushev333@gmail.com>
2021-11-23 18:57:51 +0200

    adds requests module and introduces currency_request method

--- flask-lab.py
+++ flask-lab.py
@@ -1,21 +1,25 @@
 from flask import Flask, jsonify, render_template
+import requests
 
 app = Flask(__name__)
 
 
-#
-#
-#
-#
-#
-#
-#
-
-@app.route("currency_request")
-def currency_request("get-currencies"):
+# Examples from slides
+# r = requests.put('https://httpbin.org/put', data={'key': 'value'})
+# r = requests.delete('https://httpbin.org/delete')
+# r = requests.head('https://httpbin.org/get')
+# r = requests.options('https://httpbin.org/get')
+# payload = {'key1': 'value1', 'key2': 'value2'}
+# r = requests.get('https://httpbin.org/get', params=payload)
 
 
-    return "Implement."
+@app.route("/currency_request")
+def currency_request():
+    r = requests.get('https://open.er-api.com/v6/latest/USD')
+    json = r.json()
+    print(json)
+    return json
+
 
 @app.route("/")
 def greet():
