2a2a7d3ac30e1aacf1517322fb45ebc77b36ce7e
infinite-ace <angelkukushev@infinitelambda.com>
2021-11-25 08:03:27 +0200

    Adds ZeroDivisionError exception, handles invalid urls, adds README.md, few templates for ease-of-use

---
+++ README.md
@@ -0,0 +1,8 @@
+Create a flask API which converts a number from one currency to another.
+- Use this free API to get the data from
+- Handle exceptions, by giving informative error messages
+- [Optional] Cache already loaded exchange rate
+- [Optional] Add a joke message to the end of every request using this
+API
+Hint: To reverse an exchange rate use 1/rate. E.g. 1USD = 1.71BGN, 1BGN 1/1.71USD
+ 
--- flask-lab.py
+++ flask-lab.py
@@ -4,20 +4,15 @@ import requests
 app = Flask(__name__)
 
 
-# Examples from slides
-# r = requests.put('https://httpbin.org/put', data={'key': 'value'})
-# r = requests.delete('https://httpbin.org/delete')
-# r = requests.head('https://httpbin.org/get')
-# r = requests.options('https://httpbin.org/get')
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# r = requests.get('https://httpbin.org/get', params=payload)
-
-
 @app.route("/currency_request/<amount>/<currency_from>/<currency_to>")
 def currency_request(currency_from, currency_to, amount):
+    # Use case: Changing the url to invalid will invoke the except clause
     request_url = 'https://open.er-api.com/v6/latest/' + currency_to
-    r = requests.get(request_url)
-    json = r.json()
+    try:
+        r = requests.get(request_url)
+        json = r.json()
+    except:
+        print("Request failed. Try again later.")
 
     rate = json['rates'][currency_from]
 
@@ -32,11 +27,53 @@ def currency_request(currency_from, currency_to, amount):
                            )
 
 
+# Reversed
+@app.route("/reverse_currency_request/<amount>/<currency_from>/<currency_to>")
+def reverse_currency_request(currency_from, currency_to, amount):
+    # Use case: Changing the url to invalid will invoke the except clause
+    request_url = 'https://open.er-api.com/v6/latest/' + currency_to
+    try:
+        r = requests.get(request_url)
+        json = r.json()
+    except:
+        print("Request failed. Try again later.")
+
+    rate = json['rates'][currency_from]
+
+    # Creates black holes when uncommented.
+    rate = 0
+
+    result = ""
+    try:
+        result = 1 / rate * int(amount)
+    except ZeroDivisionError:
+        result = "You just created another black with the mighty technique of dividing by zero." \
+                 " Please pick(carefully) another number to divide with."
+        print("You just created another black with the mighty technique of dividing by zero."
+              " Please pick(carefully) another number to divide with.")
+        return render_template('request_failed.html', error_message="Division By Zero. Now that's really cool!")
+
+    return render_template('conversion.html',
+                           amount=amount,
+                           rate=str(rate),
+                           currency_from=currency_from,
+                           currency_to=currency_to,
+                           result=str(result)
+                           )
+
+
 @app.route("/currency_request_json/<amount>/<currency_from>/<currency_to>")
 def currency_request_json(currency_from, currency_to, amount):
+    # Use case: Changing the url to invalid will invoke the except clause
     request_url = 'https://open.er-api.com/v6/latest/' + currency_to
-    r = requests.get(request_url)
-    json = r.json()
+
+    try:
+        r = requests.get(request_url)
+        json = r.json()
+    except:
+        print("Request failed. Try again later or make sure your url is valid.")
+        return jsonify("Bad request. The API may be down for maintenance or invalid url is passed. Check your request"
+                       "url and validate through the browser, or try again later.")
 
     rate = json['rates'][currency_from]
 
@@ -44,12 +81,23 @@ def currency_request_json(currency_from, currency_to, amount):
 
     return jsonify(result)
 
-@app.route("/available_currencies")
+
+@app.route("/")
 def available_currencies():
-    r = requests.get('https://open.er-api.com/v6/latest/USD')
-    json = r.json()
+    # Use case: Changing the url to invalid will invoke the except clause
+    request_url = "https://open.er-api.com/v6/latest/USD"
+
+    try:
+        r = requests.get(request_url)
+        json = r.json()
+    except:
+        print("Request failed. Try again later.")
+        return render_template('request_failed.html', error_message="API URL is not valid or site is down"
+                                                                    " for maintenance.")
+
     currencies = json['rates'].keys()
     currencies_list = []
+    link_to_navigate = "https://localhost:5000/currency_request/<amount>/<currency_from>/<currency_to>"
     for currency in currencies:
         currencies_list.append(currency)
 
