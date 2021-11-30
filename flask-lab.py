ee4ca4f009fb64f24f6e208f22284df585f2c65c
infinite-ace <angelkukushev@infinitelambda.com>
2021-11-30 16:07:14 +0200

    adds progress on BDD lab and imports behave & features directory

--- flask-lab.py
+++ flask-lab.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -27,7 +28,6 @@ def currency_request(currency_from, currency_to, amount):
                            )
 
 
-# Reversed
 @app.route("/reverse_currency_request/<amount>/<currency_from>/<currency_to>")
 def reverse_currency_request(currency_from, currency_to, amount):
     # Use case: Changing the url to invalid will invoke the except clause
@@ -48,7 +48,7 @@ def reverse_currency_request(currency_from, currency_to, amount):
         result = 1 / rate * int(amount)
     except ZeroDivisionError:
         err = "You just created another black with the mighty technique of dividing by zero." \
-                 " Please pick(carefully) another number to divide with."
+              " Please pick(carefully) another number to divide with."
         result = err
         print(err)
         return render_template('request_failed.html', error_message="Division By Zero. Now that's really cool!")
@@ -61,25 +61,6 @@ def reverse_currency_request(currency_from, currency_to, amount):
                            result=str(result)
                            )
 
-
-@app.route("/currency_request_json/<amount>/<currency_from>/<currency_to>")
-def currency_request_json(currency_from, currency_to, amount):
-    # Use case: Changing the url to invalid will invoke the except clause
-    request_url = 'https://open.er-api.com/v6/latest/' + currency_to
-
-    try:
-        r = requests.get(request_url)
-        json = r.json()
-    except:
-        print("Request failed. Try again later or make sure your url is valid.")
-        return jsonify("Bad request. The API may be down for maintenance or invalid url is passed. Check your request"
-                       "url and validate through the browser, or try again later.")
-
-    rate = json['rates'][currency_from]
-
-    result = rate * int(amount)
-
-    return jsonify(result)
 
 
 @app.route("/")
@@ -102,6 +83,43 @@ def available_currencies():
         currencies_list.append(currency)
 
     return render_template('available_currencies.html', currencies=currencies_list)
+
+
+#
+#     Test the CurrencyExchange API you've made
+#     1. Figure out a way to return the clean payload as JSON, instead of HTML
+#     2. Figure out a way to Make a Post Request using Python
+#     3. Make the tests using BDD Approach
+#     4. For the next step, figure out how to MOCK the response of the API
+#     5. Figure out different steps and scenarios to test.
+#     6. BONUS STEP, do everything you've done but in JAVA
+#
+
+@app.route("/currency_request_json/<amount>/<currency_from>/<currency_to>")
+def currency_request_json(currency_from, currency_to, amount):
+    # Use case: Changing the url to invalid will invoke the except clause
+    request_url = 'https://open.er-api.com/v6/latest/' + currency_to
+
+    try:
+        r = requests.get(request_url)
+        json_result = r.json()
+    except:
+        print("Request failed. Try again later or make sure your url is valid.")
+        return jsonify("Bad request. The API may be down for maintenance or invalid url is passed. Check your request"
+                       "url and validate through the browser, or try again later.")
+
+    rate = json_result['rates'][currency_from]
+
+    result = rate * int(amount)
+
+    my_json = {
+        "amount" : amount,
+        "result" : result,
+        "server_response" : 200
+    }
+
+    return json.dumps(my_json)
+
 
 
 if __name__ == '__main__':
